a0cedc2c42cf3ac17be9111df0803ea10b1d769e
Arjen <arjen@pop-os.localdomain>
2020-05-27 13:01:31 +0200

    init

---
+++ src/Compnents/Episodes/Index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Episode = () => {
+    <div>
+        episodesnp
+    </div>
+}
+
+export default Episode
---
+++ src/Compnents/Series/Index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Series = () => {
+    <div>
+        series
+    </div>
+}
+
+export default Series
