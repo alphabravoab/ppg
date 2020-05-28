462699ba9a1815c440cce9998df48acf31323b67
arjen <Alphabravoab@gmail.com>
2020-05-28 23:39:45 +0200

    made client https for netlifly

--- src/HttpClient/client.js
+++ src/HttpClient/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://api.tvmaze.com/'
+const url = 'https://api.tvmaze.com/'
 
 // generic http client call so these can be switched without breaking the app. App only expects promises.
 export const http_get = (endpoint) => { 
